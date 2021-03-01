583a4ad6c031ff132bdc58fa25ff4f2b52f74f77
fetex <dicson@javerianacali.edu.co>
2021-03-01 05:45:41 -0500

    login

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.conf import settings 
 from django.urls import path
 
-from posts import views as post_views
+from posts import views as posts_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', post_views.list_posts),
+    path('posts/', posts_views.list_posts, name='feed'),
+    path('users/login/', users_views.login_view, name='login')
 ] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
